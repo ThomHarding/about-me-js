52e310d1e336b290f53304c7122eb4b061dafc47
THOMAS HARDING <hardingt604@gmail.com>
2022-03-01 13:41:32 -0700

    Added another button to personalize page (mining)

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const showButton = document.getElementById('animalButton');
 const animal = document.getElementById('favouriteAnimalDiv');
+const mining = document.getElementById('miningIcon');
+const miningLevel = document.getElementById('miningLevel');
+const miningMaxed = document.getElementById('miningMaxed');
 
 // set event listeners
 showButton.addEventListener('click', () => {
     animal.classList.remove('hidden');
+});
+
+mining.addEventListener('click', () => {
+    if (miningLevel.innerText < 99) {
+        if (miningLevel.innerText === '98') {
+            miningMaxed.classList.remove('hidden');
+        }
+        miningLevel.innerText++;
+    }
 });
