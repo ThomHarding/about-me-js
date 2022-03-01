0d5419f179cf1d0b93b28931689be795564ebc40
THOMAS HARDING <hardingt604@gmail.com>
2022-03-01 12:11:08 -0700

    added basic javascript button functionality

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-// import functions
-
-// grab DOM elements
+const showButton = document.getElementById('animalButton');
+const animal = document.getElementById('favouriteAnimalDiv');
 
 // set event listeners 
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
+showButton.addEventListener('click'), () => {
+    animal.classList.remove('hidden');
+};
