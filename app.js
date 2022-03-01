81753bd707e23824dbb05a1cae2a1ad2af1c9c42
THOMAS HARDING <hardingt604@gmail.com>
2022-03-01 12:14:35 -0700

    fixed js functionality (minor missing parenthesis)

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const showButton = document.getElementById('animalButton');
 const animal = document.getElementById('favouriteAnimalDiv');
 
-// set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
-showButton.addEventListener('click'), () => {
+// set event listeners
+showButton.addEventListener('click', () => {
     animal.classList.remove('hidden');
-};
+});
